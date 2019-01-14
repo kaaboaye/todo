8e417203b904965f2dd10d0d9bb092428962511f
Mieszko Wawrzyniak <kaaboaye@gmail.com>
2019-01-14 16:15:31 +0100

    check origin

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :todo, TodoWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80]
+  url: [host: "example.com", port: 80],
+  check_origin: ["//localhost", "//wojtek-todo.herokuapp.com"]
 
 # Do not print debug messages in production
 config :logger, level: :info
